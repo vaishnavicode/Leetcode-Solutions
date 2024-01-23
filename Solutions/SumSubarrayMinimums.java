5dc8629d4923f7fcf154ed897df14403c66a2fdc
Vaishnavi Singh <vaishnavicode3@gmail.com>
2024-01-23 17:38:25 +0530

    Added solution and problem statement of 907

---
+++ Solutions/SumSubarrayMinimums.java
@@ -0,0 +1,35 @@
+package Solutions;
+
+public class SumSubarrayMinimums {
+    public static void main(String[] args) {
+        
+        Solution907 s = new Solution907();
+        int[] arr = {11,81,94,43,3};
+        System.out.println(s.sumSubarrayMins(arr));
+
+    }
+    
+}
+
+class Solution907 {
+    public int sumSubarrayMins(int[] arr) {
+        int[] stk = new int[arr.length+1];
+        int idx=0;
+
+        int[] dp = new int[arr.length];
+        dp[0] = arr[0];
+
+        long res=dp[0];
+        for(int i=1; i<arr.length; i++){
+            while(idx>=0 && arr[stk[idx]]>=arr[i]){
+                idx--; //popping stack top
+            }
+
+            dp[i] = idx<0?arr[i]*(i+1):dp[stk[idx]]+(arr[i]*(i-stk[idx]));
+            res += dp[i];
+            stk[++idx] = i; //pushing into stack
+        }
+
+        return (int)(res%1_000_000_007);
+    }
+}
