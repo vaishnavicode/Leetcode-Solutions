64b0edad597a5ff59784fe84359fa9f1f1d85a81
Vaishnavi Singh <vaishnavicode3@gmail.com>
2024-02-02 20:57:08 +0530

    Added solution and problem statement of 2966

---
+++ Problem Statements and Solutions/Q_2966_Arrays_With_Max_Difference/ArraysWithMaxDifference.java
@@ -0,0 +1,41 @@
+import java.util.Arrays;
+
+class ArraysWithMaxDifference{
+    public static void main(String[] args) {
+
+        Solution2966 s = new Solution2966();
+        int[] nums = {1,3,4,8,7,9,3,5,1};
+        int k = 2;
+        int[][] result = s.divideArray(nums, k);
+
+        for (int i = 0; i < result.length; ++i) {
+            System.out.println(Arrays.toString(result[i]));
+        }
+        
+    }
+}
+
+class Solution2966 {
+    public int[][] divideArray(int[] nums, int k) {
+        Arrays.sort(nums);
+        int[][] result = new int[0][0];
+        
+        for (int i = 0; i + 2 < nums.length; ++i) {
+
+            if (i % 3 == 0) {
+
+                if (nums[i + 2] - nums[i] <= k) {
+
+                    int[] triplet = {nums[i], nums[i + 1], nums[i + 2]};
+                    result = Arrays.copyOf(result, result.length + 1);
+                    result[result.length - 1] = triplet;
+                } 
+                else {
+                    return new int[0][0];
+                }
+            }
+        }
+        
+        return result;
+    }
+}
