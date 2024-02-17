4458ec95c48c09b7ca5ec418bda4b640e54a451c
Vaishnavi Singh <vaishnavicode3@gmail.com>
2024-02-17 13:26:04 +0530

    Added solution and problem statement of 2108

---
+++ Problem Statements and Solutions/Q_2108_First_Palindromic_String/FirstPalindromicString.java
@@ -0,0 +1,35 @@
+class FirstPalindromicString{
+    public static void main(String[] args) {
+
+        Solution2108 s = new Solution2108();
+        String[] words = {"abc","car","ada","racecar","cool"};
+        System.out.println(s.firstPalindrome(words));
+        
+    }
+}
+
+class Solution2108 {
+    public String firstPalindrome(String[] words) {
+        
+        for(int i =0;i<words.length;i++){
+            boolean res = pal(words[i]);
+            if(res){
+                return words[i];
+            }
+        }
+        return "";
+    }
+
+    public boolean pal(String p){
+        int n = p.length()-1;
+        int l =0;
+        while(l<n){
+            if(p.charAt(l)!=p.charAt(n)){
+                return false;
+            }
+            l++;
+            n--;
+        }
+        return true;
+    }
+}
